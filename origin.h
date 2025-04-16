8b4ff1bec3f72bad805fd038f028e08fdef335e0
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-04-16 08:43:12 +0200

    tweak origin_print

--- origin.h
+++ origin.h
@@ -12,5 +12,7 @@ static void origin_print(Origin origin) {
     printf("%.*s:%" PRIu32 ":%" PRIu32 ":%" PRIu32, (i32)origin.file_name.len,
            origin.file_name.data, origin.line, origin.column,
            origin.file_offset);
+  } else {
+    printf("(synth)");
   }
 }
