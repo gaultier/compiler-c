a6f768ce9e566e3314a1458f05122d87ecd878bf
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-04-17 14:30:22 +0200

    encode mov [rbp+disp32], reg64

--- amd64.h
+++ amd64.h
@@ -426,6 +426,33 @@ static void amd64_encode_instruction_mov(Pgu8Dyn *sb,
     return;
   }
 
+  if (AMD64_OPERAND_KIND_EFFECTIVE_ADDRESS == instruction.dst.kind &&
+      AMD64_OPERAND_KIND_REGISTER == instruction.src.kind) {
+    PG_ASSERT(instruction.dst.effective_address.base.value == amd64_rbp.value &&
+              "todo");
+
+    u8 rex = AMD64_REX_DEFAULT | AMD64_REX_MASK_W;
+    if (amd64_is_register_64_bits_only(instruction.dst.reg)) {
+      rex |= AMD64_REX_MASK_B;
+    }
+    if (amd64_is_register_64_bits_only(instruction.src.reg)) {
+      rex |= AMD64_REX_MASK_R;
+    }
+    *PG_DYN_PUSH(sb, allocator) = rex;
+
+    u8 opcode = 0x89;
+    *PG_DYN_PUSH(sb, allocator) = opcode;
+    u8 modrm =
+        (0b10 /* [rbp] + disp32 */ << 6) |
+        (u8)((amd64_encode_register_value(instruction.src.reg) & 0b111) << 3) |
+        (u8)(amd64_encode_register_value(instruction.dst.reg) & 0b111);
+    *PG_DYN_PUSH(sb, allocator) = modrm;
+
+    pg_byte_buffer_append_u32(sb, (u32)instruction.src.immediate, allocator);
+
+    return;
+  }
+
   PG_ASSERT(0 && "todo");
 }
 
