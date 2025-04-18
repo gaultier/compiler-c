8812e818d70a77f9005942c0fc9ea5e7c89408d7
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-04-18 19:44:48 +0200

    rm -fuse-ld=lld

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 set -f # disable globbing.
 
-CFLAGS="${CFLAGS} -fpie -fno-omit-frame-pointer -gsplit-dwarf -march=native -fuse-ld=lld"
+CFLAGS="${CFLAGS} -fpie -fno-omit-frame-pointer -gsplit-dwarf -march=native "
 LDFLAGS="${LDFLAGS} -Wl,--gc-sections -flto"
 
 CC="${CC:-clang}"
